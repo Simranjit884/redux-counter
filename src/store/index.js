85095227aa14b8ec5174c48f4c0e1bf75ef002f2
samy884 <ssgill884@gmail.com>
2022-06-19 13:18:30 +0530

    created authSLice

--- src/store/index.js
+++ src/store/index.js
@@ -21,8 +21,23 @@ const counterSlice = createSlice({
   }
 });
 
+const authInitialState = {isAuthenticated: false};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: authInitialState,
+  reducer: {
+    logIn(state) {
+      state.isAuthenticated = true;
+    },
+    logOut(state) {
+      state.isAuthenticated = false;
+    },
+  }
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
 });
 
 export const counterActions = counterSlice.actions;
